776584eabcafcdc27a8c59cd341e5f36eba3d4e9
dda20102004 <128996544+dda20102004@users.noreply.github.com>
2023-11-29 23:50:50 +0700

    updatesua

--- Website_DUAN1/admin/donhang/listdh.php
+++ Website_DUAN1/admin/donhang/listdh.php
@@ -25,26 +25,25 @@
             </tr>
             <?php
                 
-            foreach ($list_donhang as $dh) {
-                extract($dh);
-                $xoadh = "index.php?act=xoadh&id_dh=" . $id_dh;
-                // $suactsp = "index.php?act=updatectsp&id_ctsp=" . $id_ctsp;
-                echo '<tr>
-
-                    <td><input type="checkbox" name="" id=""></td>
-                    <td>' . $ten_nguoi_nhan . '</td>
-                    <td>' . $email . '</td>
-                    <td>' . $sdt_nguoi_nhan . '</td>
-                    <td>' . $dia_chi_nguoi_nhan . '</td>  
-                    <td>' . $name.'</td>
-                    <td>
-                        <a href="'.$xoadh.'" onclick="return confirm(\'Bạn có chắc chắn muốn xóa không?\');">
-                            <input type="button" value="Xóa">
-                        </a>
-                        <a href=""><input type="button" value="Sửa"></a>
-                    </td>
-                </tr>';
-            }
+                foreach ($list_donhang as $dh) {
+                    extract($dh);
+                    $xoadh = "index.php?act=xoadh&id_dh=" . $id_dh;
+                    $suadh = "index.php?act=suadh&id_dh=" . $id_dh;
+                    echo '<tr>
+                        <td><input type="checkbox" name="" id=""></td>
+                        <td>' . $ten_nguoi_nhan . '</td>
+                        <td>' . $email . '</td>
+                        <td>' . $sdt_nguoi_nhan . '</td>
+                        <td>' . $dia_chi_nguoi_nhan . '</td>  
+                        <td>' . $name.'</td>
+                        <td>
+                            <a href="'.$xoadh.'" onclick="return confirm(\'Bạn có chắc chắn muốn xóa không?\');">
+                                <input type="button" value="Xóa">
+                            </a>
+                            <a href="' . $suadh . '"><input type="button" value="Sửa"></a>
+                        </td>
+                    </tr>';
+                }
             ?>
         </thead>
         </table>
---
+++ Website_DUAN1/admin/donhang/updatedh.php
@@ -0,0 +1,38 @@
+<?php
+if (is_array($dh)) {
+    extract($dh);
+}
+?>
+
+<div class="container">
+    <h2>Cập nhật đơn hàng </h2>
+    <form action="index.php?act=updatedh" method="POST">
+        <div class="form-group">
+            <label for="trangthai">Trạng thái đơn hàng</label>
+            <select class="form-control" id="trangthai" name="id_trangthai">
+                <?php
+                if (isset($list_ctdonhang) && is_array($list_ctdonhang)) {
+                    foreach ($list_ctdonhang as $dh) {
+                        extract($dh);
+                        if ($id_trangthai == $id) {
+                            echo '<option value="' . $id . '" selected>' . $name . '</option>';
+                        } else {
+                            echo '<option value="' . $id . '">' . $name . '</option>';
+                        }
+                    }
+                }
+                ?>
+            </select>
+        </div>
+        <div class="mt-3 text-center">
+            <input type="hidden" name="id" value="<?php if(isset($id) && ($id > 0)) echo $id ?>">
+            <input type="submit" name="suadh" class="btn btn-outline-primary" value="Cập nhật">
+            <input type="reset" class="btn btn-outline-primary" value="Nhập lại">
+        </div>
+    </form>
+    <a href="index.php?act=donhang"><button type="button" class="btn btn-outline-primary">Danh sách <i class="fa-solid fa-right-long"></i></button></a>
+    <?php
+    if (isset($thongbao) && ($thongbao != ""))
+        echo $thongbao;
+    ?>
+</div>
